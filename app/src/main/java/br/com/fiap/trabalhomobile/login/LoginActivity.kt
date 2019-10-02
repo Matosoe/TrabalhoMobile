67cdfa93fb534510b337cebe7c06b32138ee9f47
fedacostasilva <felipe.costa@outlook.com>
2019-10-01 23:00:33 -0300

    Initial commit

---
+++ app/src/main/java/br/com/fiap/trabalhomobile/login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package br.com.fiap.trabalhomobile.login
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import br.com.fiap.trabalhomobile.R
+
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    }
+
+
