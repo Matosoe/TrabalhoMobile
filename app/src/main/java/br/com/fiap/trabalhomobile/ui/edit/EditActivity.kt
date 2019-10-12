588597fbcab746675be08d856995e62372946335
Matosoe <eduardo.matoso@gmail.com>
2019-10-12 16:35:34 -0300

    Ajuste de mais telas, Menu e Edit

--- app/src/main/java/br/com/fiap/trabalhomobile/ui/edit/EditActivity.kt
+++ app/src/main/java/br/com/fiap/trabalhomobile/ui/edit/EditActivity.kt
@@ -1,13 +1,13 @@
-package br.com.fiap.trabalhomobile.ui.cadastro
+package br.com.fiap.trabalhomobile.ui.edit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.fiap.trabalhomobile.R
 
-class CadastroActivity : AppCompatActivity() {
+class EditActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cadastro)
+        setContentView(R.layout.activity_edit)
     }
 }
--- app/src/main/java/br/com/fiap/trabalhomobile/ui/sobre/AboutActivity.kt
+++ app/src/main/java/br/com/fiap/trabalhomobile/ui/sobre/AboutActivity.kt
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.fiap.trabalhomobile.R
 
-class SobreActivity : AppCompatActivity() {
+class AboutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sobre)
+        setContentView(R.layout.activity_about)
     }
 }
